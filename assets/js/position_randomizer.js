b74c177d0212dbbbf0fed766a8de489ad87d4585
nicholaseb <nicholaseb@gmail.com>
2026-04-17 21:14:10 -0400

    Rebuild site

---
+++ assets/js/position_randomizer.js
@@ -0,0 +1,76 @@
+function pos() {
+    var title = document.getElementById("text1");
+    var pos2 = Math.floor(Math.random() * 300) + 160; // Random numbers from 200 to 500 for the position
+    var pos3 = Math.floor(Math.random() * 600) + 160; // Random numbers from 200 to 500 for the position
+    title.setAttribute(
+        "style", 
+        "color: black; position: absolute; left:" + pos2 + "px; top:" + pos3 + "px;"
+        );
+    console. log(pos2); // Log the random position to the console
+    /* console. log(pos3); // Log the random position to the console */
+
+    var title = document.getElementById("text2");
+    var pos2 = Math.floor(Math.random() * 300) + 19; // Random numbers from 200 to 500 for the position
+    var pos3 = Math.floor(Math.random() * 600) + 190; // Random numbers from 200 to 500 for the position
+    title.setAttribute(
+        "style", 
+        "color: black; position: absolute; left:" + pos2 + "px; top:" + pos3 + "px;"
+        );
+    console. log(pos2); // Log the random position to the console
+
+    var title = document.getElementById("text3");
+    var pos2 = Math.floor(Math.random() * 300) + 20; // Random numbers from 200 to 500 for the position
+    var pos3 = Math.floor(Math.random() * 600) + 200; // Random numbers from 200 to 500 for the position
+    title.setAttribute(
+        "style", 
+        "color: black; position: absolute; left:" + pos2 + "px; top:" + pos3 + "px;"
+        );
+    console. log(pos2); // Log the random position to the console
+
+    var title = document.getElementById("text4");
+    var pos2 = Math.floor(Math.random() * 300) + 18; // Random numbers from 200 to 500 for the position
+    var pos3 = Math.floor(Math.random() * 600) + 180; // Random numbers from 200 to 500 for the position
+    title.setAttribute(
+        "style", 
+        "color: black; position: absolute; left:" + pos2 + "px; top:" + pos3 + "px;"
+        );
+    console. log(pos2); // Log the random position to the console
+
+    var title = document.getElementById("text5");
+    var pos2 = Math.floor(Math.random() * 300) + 14; // Random numbers from 200 to 500 for the position
+    var pos3 = Math.floor(Math.random() * 600) + 140; // Random numbers from 200 to 500 for the position
+    title.setAttribute(
+        "style", 
+        "color: black; position: absolute; left:" + pos2 + "px; top:" + pos3 + "px;"
+        );
+    console. log(pos2); // Log the random position to the console
+
+    var title = document.getElementById("text6");
+    var pos2 = Math.floor(Math.random() * 300) + 16; // Random numbers from 200 to 500 for the position
+    var pos3 = Math.floor(Math.random() * 600) + 160; // Random numbers from 200 to 500 for the position
+    title.setAttribute(
+        "style", 
+        "color: black; position: absolute; left:" + pos2 + "px; top:" + pos3 + "px;"
+        );
+    console. log(pos2); // Log the random position to the console
+
+
+    var title = document.getElementById("text7");
+    var pos2 = Math.floor(Math.random() * 300) + 17; // Random numbers from 200 to 500 for the position
+    var pos3 = Math.floor(Math.random() * 600) + 170; // Random numbers from 200 to 500 for the position
+    title.setAttribute(
+        "style", 
+        "color: black; position: absolute; left:" + pos2 + "px; top:" + pos3 + "px;"
+        );
+    console. log(pos2); // Log the random position to the console
+
+
+
+
+
+
+
+
+
+
+}
